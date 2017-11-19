3fab07a0020d9165ec902d2cc933fff36ce38e76
corenel <xxdsox@gmail.com>
2017-11-19 16:16:49 +0800

    finished

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from utils import parse_arg, parse_music_list, find_music
+from utils import (parse_arg, parse_music_list, find_music, convert_music,
+                   check_dir)
 
 
 def main():
     """Main sciprt."""
     args = parse_arg()
     music_list = parse_music_list(args.filepath)
-    for music in music_list:
-        music_path = find_music(music['filename'], args.dir)
-        print(music_path)
-        break
+    for idx, music in enumerate(music_list):
+        filename = music['filename']
+        print('--- processing [{}/{}] {} ---'.format(
+            idx + 1, len(music_list), filename))
+        # find music file
+        filepath = find_music(filename, args.dir)
+        print('[original path]: {}'.format(filepath))
+        # convert music file
+        check_dir(args.output)
+        convert_music(filepath, args.output, dry_run=args.dry_run)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import argparse
+import subprocess
 import json
 import os
 import glob
@@ -22,6 +23,7 @@ def parse_arg():
         type=str,
         default='/Volumes/Backup/iTunes/iTunes Media/',
         help='path for music folder')
+    parser.add_argument('--dry-run', action='store_true', help='dry run mode')
 
     # parse arguments
     args = parser.parse_args()
@@ -61,14 +63,47 @@ def find_music(filename, dirname):
     if filename:
         return os.path.abspath(os.path.join(dirname, filepath[0]))
     else:
-        return None
+        return ''
 
 
-def convert_music(filepath, outpath):
+def check_dir(dirpath):
+    """Check directory and create one if it not exists.
+
+    :dirpath: path for directory to check
+
+    """
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
+
+def convert_music(filepath, outdir, dry_run=False):
     """Convert music into ALAC format with CD quality.
 
     :filepath: path for music file to be converted
     :outpath: path for output directory
 
     """
-    pass
+    if os.path.exists(filepath):
+        filename = os.path.basename(filepath)
+        outpath = os.path.join(outdir, filename)
+        # check if already converted
+        if os.path.exists(outpath):
+            print('pass')
+            return
+        # command line for xld
+        command = 'xld -f mp3 --profile mp3_320 -o "{}" "{}"'.format(
+            outpath, filepath)
+        # dey run or real
+        if not dry_run:
+            p = subprocess.Popen(
+                command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                shell=True)
+            output, errors = p.communicate()
+            if output or errors:
+                print('[output]:\n{}'.format(
+                    output.decode('utf-8') + errors.decode('utf-8')))
+
+        else:
+            print('[command]:\n{}'.format(command))
